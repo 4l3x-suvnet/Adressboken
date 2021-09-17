86bbfe0579ab64d7550409a997bb2ec4de5528af
4l3x-suvnet <alexander.auermaa@gmail.com>
2021-09-17 13:03:58 +0200

    Skeleton added

--- Program.cs
+++ Program.cs
@@ -1,47 +1,179 @@
 ﻿using System;
 
-string contacts = "";
-int numberOfPeopleInList = 0;
-
-while (true)
+namespace museum1978
 {
-    Console.Clear();
-    Console.ForegroundColor = ConsoleColor.Yellow;
-    Console.WriteLine($"~: ADRESSBOKEN ({numberOfPeopleInList} personer, {contacts.Length} tecken) :~\n");
-    Console.ResetColor();
-    Console.WriteLine("1) Visa personer");
-    Console.WriteLine("2) Lägg till person");
-    Console.WriteLine("\nQ) Avsluta 3) Rensa adressboken");
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string currentRoom = "Entrance";
+			string selection;
+			// Entrance -> Hall A, Cafe, Hall B, WC
+			// Hall A -> Entrance, Cafe
+			// Cafe -> Gift Shop, Entrance, Hall A
+			// Gift Shop -> Cafe
+			// WC -> Entrance
+			// Hall B -> Entrance
 
-    // Console.Write("\nVal: ");
-    char choice = Console.ReadKey(true).KeyChar;
+			while (true)
+			{
+				string reminderRoom = "\nYou are now at the " + currentRoom + ", what would you like to do?";
 
-    if (choice == '1')
-    {
-        Console.Clear();
-        Console.WriteLine(Environment.NewLine + contacts + Environment.NewLine + (contacts.Split("\n").Length - 1) + " personer totalt");
-        Console.ReadKey();
-    }
-    else if (choice == '2')
-    {
-        Console.Clear();
-        Console.Write("\nNamn: ");
-        string newPerson = Console.ReadLine();
-        if (!string.IsNullOrWhiteSpace(newPerson))
-        {
-            contacts += contacts.Length >= 0 ? newPerson + Environment.NewLine : newPerson;
-            numberOfPeopleInList++;
-        }
-    }
-    else if (choice == '3')
-    {
-        Console.WriteLine("\nÄr du säker på att du vill rensa adressboken? Detta går inte att ångra! (j/n)");
-        choice = Console.ReadKey(true).KeyChar;
-        if (choice == 'j')
-        {
-            contacts = "";
-            numberOfPeopleInList = 0;
-        }
-    }
-    else if (choice == 'q') Environment.Exit(0);
+				// Entrance
+				if (currentRoom == "Entrance")
+				{
+					Console.WriteLine(reminderRoom);
+					Console.WriteLine("1) Exhibition Hall 1\n2) Exhibition Hall 2\n3) Cafe\n4) WC\n5) Leave museum\n\nA) Examine Salt Sculpture\nB) Look at Cigars");
+					
+					selection = Console.ReadLine();
+
+					if (selection == "1") // Go to line 75
+					{
+						currentRoom = "Exhibition Hall 1";
+					}
+
+					else if (selection == "2") // Go to line 105
+					{
+						currentRoom = "Exhibition Hall 2";
+					}
+
+					else if (selection == "3") // Go to line 133
+					{
+						currentRoom = "Cafe";
+					}
+
+					else if (selection == "4") // Go to line 180
+					{
+						currentRoom = "WC";
+					}
+
+					else if (selection == "5")
+					{
+						Console.WriteLine("Leaving the museum...");
+						System.Threading.Thread.Sleep(2000);
+						break;
+					}
+
+					else if (selection == "A")
+					{
+						Console.WriteLine("\nYou examine the salt sculpture. What a view.");
+						currentRoom = "Entrance";
+					}
+
+					else if (selection == "B")
+					{
+						Console.WriteLine("\nYou take a look at the cigars. Smoking.");
+						currentRoom = "Entrance";
+					}
+				}
+				// Entrance done
+
+				// Exhibition Hall 1
+				else if (currentRoom == "Exhibition Hall 1")
+				{
+					Console.WriteLine(reminderRoom);
+					Console.WriteLine("1) Entrance\n2) Cafe\n\nA) Look at Saltwater Evaporate");
+					
+					selection = Console.ReadLine();
+
+					if (selection == "1") // Go to line 22
+					{
+						currentRoom = "Entrance";
+					}
+
+					if (selection == "2") // Go to line 133
+					{
+						currentRoom = "Cafe";
+					}
+
+					else if (selection == "A")
+					{
+						Console.WriteLine("\nYou have a look at Saltwater Evaporating. Super interesting...");
+					}
+				}
+
+				// Exhibition Hall 1 done
+
+				// Exhibition Hall 2
+				else if (currentRoom == "Exhibition Hall 2")
+				{
+					Console.WriteLine(reminderRoom);
+					Console.WriteLine("1) Entrance\n\nA) Pipes\nB) Pictures of Himalaya");
+
+					selection = Console.ReadLine();
+
+					if (selection == "1") // Go to line 22
+					{
+						currentRoom = "Entrance";
+					}
+
+					else if (selection == "A") 
+					{
+						Console.WriteLine("\nYou check out the pipes.");
+					}
+
+					else if (selection == "B")
+					{
+						Console.WriteLine("\nYou walk over to the pictures of Himalaya. Astonishing.");
+					}
+				}
+				// Exhibition Hall 2 done
+
+				// Cafe
+				else if (currentRoom == "Cafe")
+				{
+					Console.WriteLine(reminderRoom);
+					Console.WriteLine("\n1) Entrance\n2) Exhibition Hall 1\n3) Gift Shop");
+
+					selection = Console.ReadLine();
+
+					if (selection == "1") // Go to line 22
+					{
+						currentRoom = "Entrance";
+					}
+
+					else if (selection == "2") // Go to line 75
+					{
+						currentRoom = "Exhibition Hall 1";
+					}
+
+					else if (selection == "3") // Go to line 161
+					{
+						currentRoom = "Gift Shop";
+					}
+				}
+				// Cafe done
+
+				// Gift Shop
+				else if (currentRoom == "Gift Shop")
+				{
+					Console.WriteLine(reminderRoom);
+					Console.WriteLine("\n1) Cafe");
+
+					selection = Console.ReadLine();
+
+					if (selection == "1") // Go to line 133
+					{
+						currentRoom = "Cafe";
+					}
+				}
+				// Gift Shop done
+
+				// WC
+				else if (currentRoom == "WC")
+				{
+					Console.WriteLine(reminderRoom);
+					Console.WriteLine("\n1) Entrance");
+
+					selection = Console.ReadLine();
+
+					if (selection == "1") // Go to line 22
+					{
+						currentRoom = "Entrance";
+					}
+				}
+				// WC done
+			}
+		}
+	}
 }
